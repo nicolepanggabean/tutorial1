79b054bbd63a58ebb2ea19d2fd2b35524149d9e8
nicolepanggabeann <nicole.panggabean@gmail.com>
2022-09-08 08:41:40 +0700

    Finished tutorial 1

---
+++ wishlist/models.py
@@ -0,0 +1,7 @@
+# Create your models here.
+from django.db import models
+
+class ItemWishlist(models.Model):
+    item_name = models.CharField(max_length=50)
+    item_price = models.IntegerField()
+    description = models.TextField()
